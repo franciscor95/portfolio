f74bcdf4f5ac7761dd5dc777381deec5636294f0
Rui V Baltazar <ext.rui.baltazar@farfetch.com>
2021-04-20 09:33:14 +0100

    updates and fixes

--- js/carousel.js
+++ js/carousel.js
@@ -1,39 +1,65 @@
-const allCarousels = document.querySelectorAll('.carousel');
-const allCarouselItems = document.querySelectorAll('.carousel-item');
-
 // carousel counter
-allCarousels.forEach(carousel => {
-  const progressText = carousel.parentElement.querySelector('.image-progress p span');
+const setCounter = () => {
+  const allCarousels = document.querySelectorAll('.carousel');
+  allCarousels.forEach(carousel => {
+    const progressText = carousel.parentElement.querySelector('.image-progress p span');
+  
+    $(`#${carousel.id}`).on('slide.bs.carousel', (event) => {
+      progressText.innerText = event.to + 1;
+    })
+  });
+};
 
-  $(`#${carousel.id}`).on('slide.bs.carousel', (event) => {
-    progressText.innerText = event.to + 1;
+
+// VIDEO
+const videoCarouselItem = document.querySelector('.item-video');
+const videoCarouselParent = videoCarouselItem.parentElement.parentElement;
+const videoCarouselJQ = $(`#${videoCarouselParent.id}`);
+const videoElement = document.querySelector('video');
+// const videoElementIframe = document.querySelector('iframe').contentDocument;
+
+
+// video dimensions
+const setVideoDimensions = () => {
+  const height = document.querySelector('.main').offsetWidth / 1.5;
+
+  videoCarouselItem.style.height = `${height}px`;
+};
+
+// video behaviour
+const setVideoBehaviour = () => {
+  // const videoSetup = () => {
+  //   videoElementIframe.setAttribute('loop', 'true');
+  // };
+
+  const videoCheck = () => {
+    const videoItem = document.querySelector('.item-video');
+
+    if (videoItem.classList.contains('active')) {
+      videoCarouselJQ.carousel('pause');
+      videoElement.play();
+      videoElement.muted = false;
+      console.log('video is here!!!')
+    } else if (videoElement.currentTime !== 0) {
+      videoCarouselJQ.carousel('cycle');
+      videoElement.pause();
+      videoElement.currentTime = 0;
+      console.log('no more video :(')
+    }
+  };
+
+  videoCarouselJQ.on('slid.bs.carousel', () => {
+    videoCheck();
   })
-});
 
-// carousel dimensions
-const getDimensions = () => {
-  const column = document.querySelector('.main');
-  return {
-    width: column.offsetWidth,
-    height: column.offsetWidth / 1.5,
-    smallHeight: column.offsetWidth / 1.2,
-  }
-}
-
-const setDimensions = () => {
-  const docWidth = document.body.clientWidth;
-  const { height, smallHeight, width } = getDimensions();
-
-  const realHeight = docWidth < 768 ? smallHeight : height
-
-  allCarouselItems.forEach((item) => {
-    item.style.height = `${realHeight}px`;
-    item.style.width = `${width}px`;
-  })
+  // videoSetup()
+  videoCheck();
 }
 
 $(window).resize(() => {
-  setDimensions();
+  setVideoDimensions();
 });
 
-setDimensions();
+setCounter();
+setVideoDimensions();
+setVideoBehaviour();
