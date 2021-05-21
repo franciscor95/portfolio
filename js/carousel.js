21230d360f3a6da25c7b2f3bed03c4dac84302d0
Rui V Baltazar <ext.rui.baltazar@farfetch.com>
2021-05-21 19:31:52 +0100

    changes to video; carousels only move on focus

--- js/carousel.js
+++ js/carousel.js
@@ -1,50 +1,30 @@
-// carousel counter
-const setCounter = () => {
-  const allCarousels = document.querySelectorAll('.carousel');
-  allCarousels.forEach(carousel => {
-    const progressText = carousel.parentElement.querySelector('.image-progress p span');
-  
-    $(`#${carousel.id}`).on('slide.bs.carousel', (event) => {
-      progressText.innerText = event.to + 1;
-    })
-  });
-};
-
-
 // VIDEO
 const videoCarouselItem = document.querySelector('.item-video');
 const videoCarouselParent = videoCarouselItem.parentElement.parentElement;
 const videoCarouselJQ = $(`#${videoCarouselParent.id}`);
 const videoElement = document.querySelector('video');
-// const videoElementIframe = document.querySelector('iframe').contentDocument;
 
-
-// video dimensions
+// VIDEO DIMENSIONS
 const setVideoDimensions = () => {
   const height = document.querySelector('.main').offsetWidth / 1.5;
-
   videoCarouselItem.style.height = `${height}px`;
 };
 
-// video behaviour
-const setVideoBehaviour = () => {
-  // const videoSetup = () => {
-  //   videoElementIframe.setAttribute('loop', 'true');
-  // };
+const videoReset = () => {
+  videoElement.pause();
+  videoElement.currentTime = 0;
+};
 
+// VIDEO BEHAVIOUR
+const setVideoBehaviour = () => {
   const videoCheck = () => {
     const videoItem = document.querySelector('.item-video');
 
     if (videoItem.classList.contains('active')) {
       videoCarouselJQ.carousel('pause');
-      videoElement.play();
-      videoElement.muted = false;
-      console.log('video is here!!!')
-    } else if (videoElement.currentTime !== 0) {
+    } else {
       videoCarouselJQ.carousel('cycle');
-      videoElement.pause();
-      videoElement.currentTime = 0;
-      console.log('no more video :(')
+      videoReset();
     }
   };
 
@@ -52,14 +32,63 @@ const setVideoBehaviour = () => {
     videoCheck();
   })
 
-  // videoSetup()
   videoCheck();
 }
 
+
+
+// CAROUSEL
+const allCarousels = document.querySelectorAll('.carousel');
+const allCarouselsJQ = $('.carousel');
+const carouselPositions = [];
+let currentCarouselPosition = 0;
+
+// CAROUSEL COUNTER
+const setCounter = (carousel) => {
+  const progressText = carousel.parentElement.querySelector('.image-progress p span');
+
+  $(`#${carousel.id}`).on('slide.bs.carousel', (event) => {
+    progressText.innerText = event.to + 1;
+  })
+};
+
+// CAROUSEL SLIDE ON FOCUS
+const setCarouselSlideOnFocus = (oldIndex, newIndex) => {
+  $(`#${allCarouselsJQ[oldIndex].id}`).carousel('pause');
+  if (allCarouselsJQ[newIndex] !== videoCarouselParent) {
+    videoReset();
+    $(`#${allCarouselsJQ[newIndex].id}`).carousel('cycle');
+  } else {
+    setVideoBehaviour();
+  }
+};
+
+carouselPositions.forEach(position => {
+  if (window.scrollY >= position - 60) {
+    currentCarouselPosition = position;
+  }
+})
+
+allCarousels.forEach(carousel => {
+  setCounter(carousel);
+  carouselPositions.push(carousel.offsetTop)
+});
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY >= carouselPositions[currentCarouselPosition + 1] - 60) {
+    currentCarouselPosition += 1;
+    setCarouselSlideOnFocus(currentCarouselPosition - 1, currentCarouselPosition);
+  } else if (window.scrollY <= carouselPositions[currentCarouselPosition - 1]) {
+    currentCarouselPosition -= 1;
+    setCarouselSlideOnFocus(currentCarouselPosition + 1, currentCarouselPosition);
+  }
+});
+
+
+
+// INITIALIZERS
 $(window).resize(() => {
   setVideoDimensions();
 });
 
-setCounter();
 setVideoDimensions();
-setVideoBehaviour();
